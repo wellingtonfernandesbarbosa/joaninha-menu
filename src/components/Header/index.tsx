3e8286579130e75b4a4900a669ce7038e8f1887b
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-08-16 15:44:59 -0300

    Update font style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Joaninha from "@/images/Joaninha.png";
 import styles from "./Header.module.scss";
-import { Item } from "@/types";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.header__content}>
-        <h1>Cardáprio da Dona Joaninha</h1>
+        <h1 className={styles.header__content__title}>Cardáprio da Dona Joaninha</h1>
         <Image src={Joaninha} width={150} height={150} alt="Logo da cantina" />
       </div>
     </header>
