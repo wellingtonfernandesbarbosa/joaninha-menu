aa1b7a3b0bd6c729ab12935100cede96f8979958
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-08-20 08:42:14 -0300

    Update analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
